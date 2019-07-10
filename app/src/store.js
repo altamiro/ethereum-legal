6c89a8d63e7c844419149644a00fe3a539aef34a
Altamiro Rodrigues <altamiro27@gmail.com>
2019-07-09 22:14:07 -0300

    [UPDATE]

--- app/src/store.js
+++ app/src/store.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import nota_legal from '@/api/nota_legal'
-import auth from '@/authService'
+import Vue from "vue";
+import Vuex from "vuex";
+import nota_legal from "@/api/nota_legal";
+import auth from "@/authService";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     sidebar: true,
     loading: false,
-    usuario: auth.get().descricao
+    usuario: auth.isAuthenticated() ? auth.get().descricao : null
   },
   mutations: {
-    TOGGLE_SIDEBAR: (state) => {
+    TOGGLE_SIDEBAR: state => {
       state.sidebar = !state.sidebar;
     },
-    CLOSE_SIDEBAR: (state) => {
+    CLOSE_SIDEBAR: state => {
       state.sidebar = false;
     },
-    TOGGLE_LOADING: (state) => {
+    TOGGLE_LOADING: state => {
       state.loading = !state.loading;
     },
-    CLOSE_LOADING: (state) => {
+    CLOSE_LOADING: state => {
       state.loading = false;
     },
     updateSidebar(state, payload) {
@@ -29,81 +29,114 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    criar_auditor(context, data){
+    criar_auditor(context, data) {
       return new Promise((resolve, reject) => {
-        nota_legal.methods.criar_auditor(data.address, data.nome, data.cpf).send({ from: data.address, gas: 3000000 }).then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(error);
-        });
+        nota_legal.methods
+          .criar_auditor(data.address, data.nome, data.cpf)
+          .send({ from: data.address, gas: 3000000 })
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
       });
     },
-    listar_auditor(context, data){
+    listar_auditor(context, data) {
       return new Promise((resolve, reject) => {
-        nota_legal.methods.listar_auditor(data.address).call({ from: data.address, gas: 3000000 }).then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(error);
-        });
+        nota_legal.methods
+          .listar_auditor(data.address)
+          .call({ from: data.address, gas: 3000000 })
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
       });
     },
-    criar_contribuinte(context, data){
+    criar_contribuinte(context, data) {
       return new Promise((resolve, reject) => {
-        nota_legal.methods.criar_contribuinte(data.address, data.cpf_cnpj).send({ from: data.address, gas: 3000000 }).then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(error);
-        });
+        nota_legal.methods
+          .criar_contribuinte(data.address, data.cpf_cnpj)
+          .send({ from: data.address, gas: 3000000 })
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
       });
     },
-    atualizar_contribuinte(context, data){
+    atualizar_contribuinte(context, data) {
       return new Promise((resolve, reject) => {
-        nota_legal.methods.atualizar_contribuinte(data.address, data.credito, data.indicacao, data.utilizado).send({ from: data.address, gas: 3000000 }).then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(error);
-        });
+        nota_legal.methods
+          .atualizar_contribuinte(
+            data.address,
+            data.credito,
+            data.indicacao,
+            data.utilizado
+          )
+          .send({ from: data.address, gas: 3000000 })
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
       });
     },
-    listar_contribuinte(context, data){
+    listar_contribuinte(context, data) {
       return new Promise((resolve, reject) => {
-        nota_legal.methods.listar_contribuinte(data.address).call({ from: data.address, gas: 3000000 }).then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(error);
-        });
+        nota_legal.methods
+          .listar_contribuinte(data.address)
+          .call({ from: data.address, gas: 3000000 })
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
       });
     },
-    adicionar_compra(context, data){
+    adicionar_compra(context, data) {
       return new Promise((resolve, reject) => {
-        nota_legal.methods.adicionar_compra(data.contribuinte_address, data.tipo, data.data, data.valor, data.tributo, data.credito, data.bilhete).send({ from: data.address, gas: 3000000 }).then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(error);
-        });
+        nota_legal.methods
+          .adicionar_compra(
+            data.contribuinte_address,
+            data.tipo,
+            data.data,
+            data.valor,
+            data.tributo,
+            data.credito,
+            data.bilhete
+          )
+          .send({ from: data.address, gas: 3000000 })
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
       });
     },
-    listar_compras(context, data){
+    listar_compras(context, data) {
       return new Promise((resolve, reject) => {
-        nota_legal.methods.listar_compras().call({ from: data.address, gas: 3000000 }).then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(error);
-        });
+        nota_legal.methods
+          .listar_compras()
+          .call({ from: data.address, gas: 3000000 })
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
       });
     }
   },
   getters: {
     sidebar: state => state.sidebar,
     loading: state => state.loading,
-    usuario: state => state.usuario,
+    usuario: state => state.usuario
   }
-})
-
+});
